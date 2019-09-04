8cee17f984ed7f56bb5c4a372b1ecb8a0b18d660
Cristian Groza <cristian.groza@protonmail.com>
2019-09-04 10:56:58 -0400

    Update.

--- predict_fifa2018.R
+++ predict_fifa2018.R
@@ -31,29 +31,43 @@ home.goals <- train %>%  select(Home_Team_Name,  Home_Team_Goals) %>% rename(Goa
 away.goals <- train %>%  select(Away_Team_Name,  Away_Team_Goals) %>% rename(Goals = Away_Team_Goals, Team = Away_Team_Name)
 
 ## calculate goal and game stats for every team, both at home and away
-goals <- rbind(home.goals, away.goals) %>% group_by(Team) %>% summarise(TotalGoals = sum(Goals), Games = n()) %>% drop_na()
+goals <- rbind(home.goals, away.goals) %>% group_by(Team) %>% summarise(TotalGoals = sum(Goals), Games = n()) %>% drop_na() %>% mutate(GoalsPerGame = TotalGoals/Games)
 
 ## Calculate Win/Loss outcomes. Draws are randomized since we want to apply logistic regresson
 data <- train %>% select(Home_Team_Name, Away_Team_Name, Home_Team_Goals, Away_Team_Goals) %>%
-  mutate(Outcome = ifelse(Away_Team_Goals > Home_Team_Goals, 0, ifelse(Home_Team_Goals > Away_Team_Goals, 1, sample(c(0,1), 1))))
+  mutate(Outcome = ifelse(Away_Team_Goals > Home_Team_Goals, "Loss", ifelse(Home_Team_Goals > Away_Team_Goals, "Win", "Tie")))
+
+
+home.winrate <- data %>% arrange(Home_Team_Name) %>% group_by(Home_Team_Name, Outcome) %>% summarise(Number = n()) %>% spread(Outcome, Number) %>% mutate(winrate = Win/(Loss + Tie))
 
 ## calculate the total games and total goals of each team in each game
 data1 <- merge(data, goals, by.x="Home_Team_Name", by.y="Team")
 data2 <-merge(data1, goals, by.x="Away_Team_Name", by.y="Team") %>%
-  rename(HomeTotalGoals = TotalGoals.x, HomeTotalGames=Games.x, AwayTotalGoals=TotalGoals.y, AwayTotalGames=Games.y)
+  rename(HomeTotalGoals=TotalGoals.x, HomeTotalGames=Games.x, AwayTotalGoals=TotalGoals.y, AwayTotalGames=Games.y, HomeGoalsPerGame=GoalsPerGame.x, AwayGoalsPerGame =GoalsPerGame.y)
 
 ## generalized linear models
-library(glmm)
-
+library(randomForest)
 ## model fit
-model <- glm(Outcome ~ HomeTotalGames + HomeTotalGoals + AwayTotalGames + AwayTotalGoals, family=binomial(link='logit'),data=data2)
+## model <- glm(Outcome ~ HomeTotalGames + HomeTotalGoals + AwayTotalGames + AwayTotalGoals, family=binomial(link='logit'),data=data2)
 ## barf... they don't predict sauat
-summary(model)
+## summary(model)
 
+## Make outcome a factor
 data2$Outcome <- as.factor(data2$Outcome)
-randomForest(Outcome ~ HomeTotalGames + HomeTotalGoals + AwayTotalGames + AwayTotalGoals, data=data2)
+## save model in forest
+forest <- randomForest(
+  Outcome ~ HomeTotalGames + HomeTotalGoals +
+    AwayTotalGames + AwayTotalGoals +
+    AwayGoalsPerGame + HomeGoalsPerGame,
+  data=data2,
+  ntree = 500)
 
 ## Confusion matrix:
 ## 0   1 class.error
 ## 0 60 114  0.65517241
 ## 1 65 613  0.09587021
+
+## prepare to predict on 2018 data
+ ## matches2018 %>% mutate(Outcome )
+
+
